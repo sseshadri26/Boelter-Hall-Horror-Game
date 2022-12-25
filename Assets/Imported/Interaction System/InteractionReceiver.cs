c7cdf9c8b14e98f86399a1a072b2a2d52cc95413
Cole Strain <colestrain2000@gmail.com>
2022-12-25 00:03:59 -0800

    Revert "Revert "Merge branch 'main' of https://github.com/sseshadri26/Boelter-Hall-Horror-Game""
    This reverts commit 69b6d477bbfeecf63d972c053908f03124c89475.

---
+++ Assets/Imported/Interaction System/InteractionReceiver.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class InteractionReceiver : MonoBehaviour
+{
+
+    /*ESPAÑOL
+     *Solución por GameDevTraum
+    * 
+    * Artículo: https://gamedevtraum.com/gdt-short/sistema-de-interaccion-base-para-unity/
+    * Página: https://gamedevtraum.com/es/
+    * Canal: https://youtube.com/c/GameDevTraum
+    * 
+    * Visita la página para encontrar más soluciones, Assets y artículos
+   */
+
+    /*ENGLISH
+    *Solution by GameDevTraum
+    * 
+    * Article: https://gamedevtraum.com/gdt-short/basic-interaction-system-for-unity/
+    * Website: https://gamedevtraum.com/en/
+    * Channel: https://youtube.com/c/GameDevTraum
+    * 
+    * Visit the website to find more articles, solutions and assets
+    */
+
+    /*DEUTSCH
+    *Lösung von GameDevTraum
+    * 
+    * Artikel: https://gamedevtraum.com/gdt-short/grundlegendes-interaktionssystem-fuer-unity/
+    * Webseite: https://gamedevtraum.com/de/
+    * Kanal: https://youtube.com/c/GameDevTraum
+    * 
+    * Besuch die Website, um weitere Artikel, Lösungen und Hilfsmittel zu finden. 
+    *
+    */
+
+    [SerializeField]
+    private string interactMessage;
+
+    [SerializeField]
+    private GameObject[] objectsWithActions;
+
+
+    public void Activate()
+    {
+        
+        foreach (GameObject o in objectsWithActions) {
+            o.GetComponent<IAction>().Activate();
+        }
+
+    }
+
+    public string GetInteractionMessage()
+    {
+        return interactMessage;
+    }
+}
