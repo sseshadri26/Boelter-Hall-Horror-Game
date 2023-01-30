851249f95c150c5db4abafdec33c8d19ef51b1a5
Alexis Packer <lexi.packer@gmail.com>
2023-01-30 15:56:36 -0800

    if player enters the trigger, disable player camera and enable jumpscare camera

---
+++ Assets/Scripts/CameraAnimation.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class CameraAnimation : MonoBehaviour
+{
+    public GameObject playerCamera;
+    public GameObject jumpCamera;
+
+    private void Start()
+    {
+        playerCamera.SetActive(true);
+        jumpCamera.SetActive(false);
+        if (playerCamera.activeSelf)
+        {
+            UnityEngine.Debug.Log("player cam active");
+        }
+    }
+
+    // if player enters trigger, set jump cam active and player cam inactive
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerCamera.SetActive(false);
+            jumpCamera.SetActive(true);
+            UnityEngine.Debug.Log("jump cam active");
+        }
+    }
+}
