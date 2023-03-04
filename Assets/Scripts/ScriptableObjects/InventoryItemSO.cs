d90f6ecfbed135a523765d556b5df19ee6807ce4
Aaron Isara <68300841+aaisara12@users.noreply.github.com>
2023-03-04 01:07:39 -0800

    Fixed glitchy highlighting in inventory panel

---
+++ Assets/Scripts/ScriptableObjects/InventoryItemSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/Inventory Item")]
+public class InventoryItemSO : ScriptableObject
+{
+    public string itemName;
+
+    [TextArea(3, 10)]
+    public string description;
+    public Sprite graphic;
+}
---
+++ Assets/Scripts/UI/InventoryUI.cs
@@ -0,0 +1,152 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class InventoryUI : MonoBehaviour
+{
+    [SerializeField] UIDocument document = default;
+    [SerializeField] VisualTreeAsset inventoryItemUI = default;
+    [SerializeField] InventorySO inventory = default;
+
+    // UI Tags
+    const string k_ItemList = "item-list";
+    const string k_ItemTitle = "item-title";
+    const string k_ItemVisual = "item-visual";
+    const string k_ItemDesc = "item-desc";
+
+    // UI Tags (Inventory Item)
+    const string k_ItemRoot = "Main";
+    const string k_ItemName = "item-name";
+    const string k_ItemGraphic = "item-graphic";
+    const string k_ItemTint = "item-tint";
+
+
+
+    // UI USS Classes
+    const string c_InventoryItemClass = "inventory-item";
+    const string c_InventoryItemTintClass = "inventory-item-tint";
+    const string c_InventoryItemTintSelectedClass = "inventory-item-tint-selected";
+
+    
+
+    // UI References
+    VisualElement m_Root = default;
+    ScrollView m_ItemList = default;
+
+    Label m_ItemTitle = default;
+    VisualElement m_ItemVisual = default;
+    Label m_ItemDesc = default;
+
+    private void Awake()
+    {
+        if(document != null)
+            m_Root = document.rootVisualElement;
+
+        m_ItemList = m_Root.Q<ScrollView>(k_ItemList);
+
+        m_ItemTitle = m_Root.Q<Label>(k_ItemTitle);
+        m_ItemVisual = m_Root.Q<VisualElement>(k_ItemVisual);
+        m_ItemDesc = m_Root.Q<Label>(k_ItemDesc);
+
+        UpdateDisplay(inventory.items);
+    }
+
+    public void OpenInventory()
+    {
+        UpdateDisplay(inventory.items);
+        // TODO: Fade inventory in
+    }
+
+    public void CloseInventory()
+    {
+        // TODO: Fade inventory out
+    }
+
+    private void UpdateDisplay(List<InventoryItemSO> itemList)
+    {
+        bool isFirstItem = true;
+        foreach(InventoryItemSO item in itemList)
+        {
+            TemplateContainer instance = inventoryItemUI.Instantiate();
+            InitializeItemElement(instance, item);
+
+            if(isFirstItem)
+            {
+                HandleInventoryItemClicked(instance, item);
+                isFirstItem = false;
+            }
+                
+            m_ItemList.Add(instance);
+
+        }
+    }
+
+    private void InitializeItemElement(VisualElement item, InventoryItemSO itemData)
+    {
+        item.Q<Label>(k_ItemName).text = itemData.itemName;
+        item.Q<VisualElement>(k_ItemGraphic).style.backgroundImage = new StyleBackground(itemData.graphic);
+
+        // Reset item highlight
+        item.Q<VisualElement>(k_ItemTint).ClearClassList();
+        item.Q<VisualElement>(k_ItemTint).AddToClassList(c_InventoryItemTintClass);
+
+        // DESIGN CHOICE: Store item reference in callback instead of using generic
+        // callback and searching for item index within function to reduce chances
+        // of erroneously selecting wrong item
+        item.RegisterCallback<ClickEvent>(ev => HandleInventoryItemClicked(item, itemData));
+    }
+
+    private void HandleInventoryItemClicked(VisualElement item, InventoryItemSO itemData)
+    {
+        // Visually select item
+        HighlightItem(item);
+
+        DisplayItemInformation(itemData);
+    }
+
+
+    ///<summary>
+    /// Wrapper around HighlightElement to ensure consistent access of item tint element
+    ///</summary>
+    private void HighlightItem(VisualElement item)
+    {
+        if(item != null)
+            HighlightElement(item.Q<VisualElement>(k_ItemTint));
+    }
+
+    private void HighlightElement(VisualElement elem)
+    {
+        if(elem != null)
+        {
+            // Unhighlight all currently selected items
+            m_Root.Query<VisualElement>(k_ItemTint).
+                Where(i => i.ClassListContains(c_InventoryItemTintSelectedClass)).
+                ForEach(UnhighlightElement);
+
+            // Highlight the desired item
+            elem.AddToClassList(c_InventoryItemTintSelectedClass);
+        }
+            
+    }
+
+    private void UnhighlightElement(VisualElement elem)
+    {
+        if(elem != null)
+            elem.RemoveFromClassList(c_InventoryItemTintSelectedClass);
+    }
+
+    private UQueryBuilder<VisualElement> GetInventoryItems() => m_Root.Query<VisualElement>(k_ItemRoot);
+
+
+    ///<summary>
+    /// Displays the given item in the inventory's main panel.
+    ///</summary>
+    private void DisplayItemInformation(InventoryItemSO item)
+    {
+        m_ItemTitle.text = item.itemName;
+        m_ItemDesc.text = item.description;
+        m_ItemVisual.style.backgroundImage = new StyleBackground(item.graphic);
+    }
+}
