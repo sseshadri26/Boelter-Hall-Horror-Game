b7a7895c9be6a74a1193ae2fa4f0749e8567e866
Alexis Packer <lexi.packer@gmail.com>
2023-03-29 17:27:32 -0700

    final push for a bit (pls don't test kinda buggy)

--- Assets/Scripts/CameraAnimation.cs
+++ Assets/Scripts/CameraAnimation.cs
@@ -12,27 +12,31 @@ public class CameraAnimation : MonoBehaviour
 {
     [SerializeField] private GameObject mainCam;
     [SerializeField] private GameObject _cutscene;
+    [SerializeField] private GameObject trigger;
     bool scenePlayed = false;
+    
 
     private void OnTriggerEnter(Collider other)
     {
         UnityEngine.Debug.Log("Player entered trigger");
         if (other.tag == "Player" && scenePlayed == false)
-        {
+        {            
             mainCam.gameObject.SetActive(false);
             _cutscene.SetActive(true);
             scenePlayed = true;
         }
+        trigger.gameObject.SetActive(false);
     }
 
     private void Update()
     {
         if (scenePlayed == true && !_cutscene.transform.GetChild(0).gameObject.activeInHierarchy)
         {
-            //mainCam.transform.position = _cutscene.transform.GetChild(0).transform.position;
+         
+            mainCam.transform.position = _cutscene.transform.GetChild(0).transform.position;
             mainCam.gameObject.SetActive(true);
             scenePlayed = false;
-       
-        }
+            //UnityEngine.Debug.Log(scenePlayed);
+        }      
     }
 }
