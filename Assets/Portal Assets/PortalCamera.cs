89063d3d23eab07fb6a46a36d967c9e5dd7dc64a
Sudarshan Seshadri <sudarshanseshadri26@gmail.com>
2023-04-30 03:10:30 -0700

    modified 2nd floor, added elevators

--- Assets/Portal Assets/PortalCamera.cs
+++ Assets/Portal Assets/PortalCamera.cs
@@ -8,8 +8,10 @@ public class PortalCamera : MonoBehaviour
     public Camera playerCamera;
     public Transform portal;
     public Transform otherPortal;
-    //private Camera portalCamera;
+    public bool visible;
+    [SerializeField] private Camera portalCamera;
 
+    [SerializeField] private MeshRenderer meshRenderer;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,17 +22,37 @@ public class PortalCamera : MonoBehaviour
             //Debug.Log("Player camera not set in editor, setting to Camera.main");
             playerCamera = Camera.main;
         }
+        // portalCamera = GetComponent<Camera>();
+        //if not on screen, don't render
+
+
     }
 
     //Update is called once per frame
     void LateUpdate()
     {
+
+
         Vector3 playerOffsetFromPortal = Quaternion.Inverse(otherPortal.rotation) * (playerCamera.transform.position - otherPortal.position);
 
         transform.localPosition = playerOffsetFromPortal;
 
 
         transform.localRotation = Quaternion.Inverse(otherPortal.rotation) * playerCamera.transform.rotation;
+
+        if (meshRenderer != null)
+        {
+            if (!meshRenderer.isVisible)
+            {
+                visible = false;
+                portalCamera.enabled = false;
+            }
+            else
+            {
+                visible = true;
+                portalCamera.enabled = true;
+            }
+        }
 
 
     }
