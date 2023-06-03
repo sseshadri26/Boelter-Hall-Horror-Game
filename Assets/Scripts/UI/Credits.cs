cd0899d4805ae282bc2a259d9f76a926c95315c2
Cole Strain <colestrain2000@gmail.com>
2023-06-03 12:31:44 -0700

    Credits and connect it to Stairwell and MainMenu

---
+++ Assets/Scripts/UI/Credits.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class Credits : MonoBehaviour
+{
+    public float endPosY = 1000f;
+    public float duration = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f;
+        GetComponent<RectTransform>().DOAnchorPosY(endPosY, duration).SetEase(Ease.Linear).OnComplete(() => SceneManager.LoadScene(0));
+    }
+}
