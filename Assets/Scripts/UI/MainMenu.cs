c2fc6954d67a36ef0a1612f55f57a11c2dc5bed0
Cole Strain <colestrain2000@gmail.com>
2023-06-02 17:51:55 -0700

    Main Menu created

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private Image blackScreen;
+    private AudioClip pop;
+
+    private void Awake()
+    {
+        // YarnCommands.SaveString("Dwayne", "false");
+        // pop = Resources.Load<AudioClip>("SFX/Pop");
+        if (SaveSystem.Data == null)
+        {
+            GameObject load = GameObject.Find("LoadButton");
+            load.GetComponent<Button>().interactable = false;
+            // load.GetComponent<Image>().color = Color.gray;
+        }
+    }
+
+    public void NewGame()
+    {
+        // MusicPlayer.audioSource.PlayOneShot(pop);
+        // if (PlayerPrefs.GetString("MainSave", "") == "")
+        // {
+        //     blackScreen.DOFade(1f, 0.5f).OnComplete( () => StartCoroutine(Click.LoadYarnScene("NewGame")));
+        // }
+        // else
+        // {
+        //     blackScreen.DOFade(1f, 0.5f).OnComplete( () => StartCoroutine(Click.LoadYarnScene("Intro")));
+        // }
+        
+        blackScreen.DOFade(1f, 0.5f).OnComplete( () => SceneManager.LoadScene("Entrance"));
+    }
+
+    public void LoadGame()
+    {
+        // MusicPlayer.audioSource.PlayOneShot(pop);
+        // Inventory.LoadGame();
+        // blackScreen.DOFade(1f, 0.5f).OnComplete( () => { 
+        //     SceneManager.LoadScene("MapScene");
+        //     YarnCommands.PlayMusic("Music/Gameplay");
+        // });
+
+        blackScreen.DOFade(1f, 0.5f).OnComplete( () => SceneManager.LoadScene(SaveSystem.Data.playerScene));
+    }
+
+    public void QuitGame()
+    {
+        // MusicPlayer.audioSource.PlayOneShot(pop);
+        #if (UNITY_EDITOR)
+            UnityEditor.EditorApplication.isPlaying = false;
+        #elif (UNITY_WEBGL)
+            Application.OpenURL("https://olaycolay.itch.io/");
+        #else
+            Application.Quit();
+        #endif
+    }
+}
