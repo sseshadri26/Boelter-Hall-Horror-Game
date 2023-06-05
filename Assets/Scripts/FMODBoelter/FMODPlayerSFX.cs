9ed3d6853b352de745dd2f0d87374f7f4f68a85a
Cole Strain <colestrain2000@gmail.com>
2023-06-05 16:36:07 -0700

    FMOD Hallway fix

--- Assets/Scripts/FMODBoelter/FMODPlayerSFX.cs
+++ Assets/Scripts/FMODBoelter/FMODPlayerSFX.cs
@@ -36,7 +36,14 @@ public class FMODPlayerSFX : MonoBehaviour
             case "Intro":
             case "5F Hallway":
                 // Haunted Hallways music_opening 0.4
-                FMODManager.Instance.ChangeMainBGM(FMODManager.SFX.music_opening, 0.2f);
+                if (Globals.curSpawnPoint == 0)
+                {
+                    FMODManager.Instance.ChangeMainBGM(FMODManager.SFX.music_opening, 0.2f);
+                }
+                else
+                {
+                    goto default;
+                }
                 break;
             case "5272":
                 // Muddled Thoughts music_5272 0.3
@@ -59,6 +66,11 @@ public class FMODPlayerSFX : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (player == null)
+        {
+            return;
+        }
+
         prevSpeed3 = prevSpeed2;
         prevSpeed2 = prevSpeed1;
         Vector3 horizontalVelocity = new Vector3(player.rb.velocity.x, 0, player.rb.velocity.z);
